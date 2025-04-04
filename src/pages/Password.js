9690cdeb86932a3eeaed4090879849c3468351b8
Yuhi0120 <yuhi.142857@akane.waseda.jp>
2025-04-04 14:25:39 -0500

    styled the password page

--- src/pages/Password.js
+++ src/pages/Password.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Password.css';
+
 
 const Password = () => {
     const [password, setPassword] = useState('');
